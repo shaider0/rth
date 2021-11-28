535c56ff21b66fe1ce2e6c0e6a8ab28f39b30398
SHaider0 <Samnoon.Haider@gmail.com>
2021-11-28 11:48:55 -0500

    Commit two

--- src/components/content.js
+++ src/components/content.js
@@ -7,10 +7,37 @@ const StyledContent = styled.div`
     padding: 10px;
 `
 
+const StyledHeader = styled.h1`
+    margin-bottom: 20px;
+`
+
+const StyledList = styled.ul`
+    text-align: left;
+    list-style: none;
+    font-size: 18px;
+`
+
+const StyledListItem = styled.li`
+    padding-bottom: 10px;
+    padding-left: 10px;
+`
+
+const StyledLink = styled.a`
+    color: #cccccc;
+    text-decoration: none;
+    :hover {
+        color: red;
+    }
+`
+
 export const Content = () => {
     return (
         <StyledContent>
-            <h1>RTH Sports & Leadership</h1>
+            <StyledHeader>RTH Sports & Leadership</StyledHeader>
+            <StyledList>
+                <StyledListItem><StyledLink href="https://www.google.com">Education</StyledLink></StyledListItem>
+                <StyledListItem><StyledLink href="https://www.google.com">Basketball</StyledLink></StyledListItem>
+            </StyledList>
         </StyledContent>
     )
 }
