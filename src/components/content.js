1ba51c8a321effcd77ff271de5e8b19b6e0f19cc
SHaider0 <Samnoon.Haider@gmail.com>
2021-11-28 11:19:51 -0500

    First commit

---
+++ src/components/content.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components'
+
+const StyledContent = styled.div`
+    background-color: #282c34;
+    color: white;
+    padding: 10px;
+`
+
+export const Content = () => {
+    return (
+        <StyledContent>
+            <h1>RTH Sports & Leadership</h1>
+        </StyledContent>
+    )
+}
